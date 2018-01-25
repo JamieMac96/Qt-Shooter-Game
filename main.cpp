1fa232fe20b3513ab2decb954a6810a03831cb07
JamieMac96 <jamiemac96@hotmail.com>
2018-01-25 19:40:30 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <QApplication>
+#include <QGraphicsItem>
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+/**/
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsScene *scene = new QGraphicsScene();
+
+    QGraphicsRectItem *rect = new QGraphicsRectItem();
+    rect->setRect(0, 0, 100, 100);
+
+    scene->addItem(rect);
+
+    QGraphicsView *view = new QGraphicsView(scene);
+    view->show();
+
+    return a.exec();
+}
